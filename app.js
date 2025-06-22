ad1f00e60ad16ec7e8cb5bf8cfca44a86e660aff
Mostafa-Nezar <mostafanezar19@gmail.com>
2025-06-22 11:04:49 +0300

    cnnect mongoose

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express')
+const mongoose = require("mongoose")
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/hi', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-app.get('/hi2', (req, res) => {
-  res.sendFile('./views/home.html',{root:__dirname})
-})
+
+mongoose.connect("mongodb+srv://mostafanezar:1234@mybackendnodejs.qhd2pij.mongodb.net/sasa-data?retryWrites=true&w=majority&appName=mybackendnodejs").then(() => {
+  app.get('/', (req, res) => {
+    res.sendFile('./views/home.html', { root: __dirname })
+  })
+
+}
+)
+
 app.listen(port, () => {
   console.log(`http://localhost:3001`)
 })
